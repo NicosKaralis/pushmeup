d4a11124a8b1cd504a2985929f1b978317c86414
Chris Mason <chris@chaione.com>
2013-10-09 13:19:35 -0500

    Revert "Change pem to pem_data, which require the cert-pem content directly."
    This reverts commit 9edd54ebd64456c2eddd2593e38c6079c9f3c6e3.

--- lib/pushmeup/apns/core.rb
+++ lib/pushmeup/apns/core.rb
@@ -7,31 +7,29 @@ module APNS
   @host = 'gateway.sandbox.push.apple.com'
   @port = 2195
   # openssl pkcs12 -in mycert.p12 -out client-cert.pem -nodes -clcerts
-  @pem_data = nil
-  # this should the content of the pem-cert(String format)
-  # changed this because all certs are stored in database
+  @pem = nil # this should be the path of the pem file not the contentes
   @pass = nil
-
+  
   class << self
-    attr_accessor :host, :pem_data, :port, :pass
+    attr_accessor :host, :pem, :port, :pass
   end
-
+  
   def self.send_notification(device_token, message)
     n = APNS::Notification.new(device_token, message)
     self.send_notifications([n])
   end
-
+  
   def self.send_notifications(notifications)
     sock, ssl = self.open_connection
-
+    
     notifications.each do |n|
-      ssl.write(n.packaged_notification)
-    end
+        ssl.write(n.packaged_notification)
+      end
 
     ssl.close
     sock.close
   end
-
+  
   def self.feedback
     sock, ssl = self.feedback_connection
 
@@ -49,34 +47,43 @@ module APNS
     return apns_feedback
   end
 
-protected
-  def self.build_connection_context
-    raise "Your don't have valid pem data. (APNS.pem = Certificate.development_pem)" unless pem_data
-    context      = OpenSSL::SSL::SSLContext.new
-    context.cert = OpenSSL::X509::Certificate.new(self.pem_data)
-    context.key  = OpenSSL::PKey::RSA.new(self.pem_data, self.pass)
-    context
+  protected
+
+  def self.pem_data
+    raise "Your pem file is not set. (APNS.pem = /path/to/cert.pem or object that responds to read)" unless pem
+    if pem.respond_to? :read
+      self.pem.read
+    else
+      raise "The path to your pem file does not exist!" unless File.exist?(@pem)
+      File.read(self.pem)
+    end
   end
 
   def self.open_connection
-    context = self.build_connection_context
-    sock    = TCPSocket.new(self.host, self.port)
-    ssl     = OpenSSL::SSL::SSLSocket.new(sock,context)
+    context      = OpenSSL::SSL::SSLContext.new
+    context.cert = OpenSSL::X509::Certificate.new(self.pem_data)
+    context.key  = OpenSSL::PKey::RSA.new(self.pem_data, self.pass)
+
+    sock         = TCPSocket.new(self.host, self.port)
+    ssl          = OpenSSL::SSL::SSLSocket.new(sock,context)
     ssl.connect
 
     return sock, ssl
   end
-
+  
   def self.feedback_connection
-    context = self.build_connection_context
+    context      = OpenSSL::SSL::SSLContext.new
+    context.cert = OpenSSL::X509::Certificate.new(self.pem_data)
+    context.key  = OpenSSL::PKey::RSA.new(self.pem_data, self.pass)
+    
     fhost = self.host.gsub('gateway','feedback')
     puts fhost
-
+    
     sock         = TCPSocket.new(fhost, 2196)
     ssl          = OpenSSL::SSL::SSLSocket.new(sock, context)
     ssl.connect
 
     return sock, ssl
   end
-
+  
 end
