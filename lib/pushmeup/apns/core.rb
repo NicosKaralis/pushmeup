4952c23991ecf7da3f8b62e80c6e89243df36a09
Lee Quarella <lee@lucidfrog.com>
2014-03-28 11:03:42 -0400

    fixed feedback method to utilize alternate pems

--- lib/pushmeup/apns/core.rb
+++ lib/pushmeup/apns/core.rb
@@ -49,7 +49,8 @@ module APNS
     end
   end
 
-  def self.feedback
+  def self.feedback(alternate_pem=nil)
+    set_pem_contents(alternate_pem)
     sock, ssl = self.feedback_connection
 
     apns_feedback = []
