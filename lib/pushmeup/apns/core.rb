c6d24fc25a7834fa0bde096422a1365a63226e4b
Pedro Piñera <pepibumur@gmail.com>
2014-08-05 17:29:56 +0200

    Added socket extra checking

--- lib/pushmeup/apns/core.rb
+++ lib/pushmeup/apns/core.rb
@@ -8,30 +8,38 @@ module APNS
     #Accessors
     attr_accessor :host, :pem, :port, :pass, :app_id
     
+    # Socket attributes
+    @sock = nil
+    @ssl = nil
+
+    # Persistance attributes
+    @persistent = false
+    @mutex = Mutex.new
+
     # Init method
-    def initialize (host='gateway.sandbox.push.apple.com', pem=nil, port=2195, pass=nil)
+    def initialize (host='gateway.sandbox.push.apple.com', pem=nil, port=2195, pass=nil, persistent = false)
       @host = host unless host == nil
       @pem = pem unless pem == nil
       @port = port unless port == nil
       @pass = pass unless pass == nil
+      @persistent = persistent
     end
 
-    #Send notification 
+    # Send notification 
     def send_notification(device_token, message)
       n = APNS::Notification.new(device_token, message)
       self.send_notifications([n])
     end
     
-    #Send notifications
+    # Send notifications
     def send_notifications(notifications)
-      sock, ssl = self.open_connection
-      
-      notifications.each do |n|
-          ssl.write(n.packaged_notification)
+      @mutex.synchronize do
+        self.with_connection do
+          notifications.each do |n|
+            @ssl.write(n.packaged_notification)
+          end
         end
-
-      ssl.close
-      sock.close
+      end
     end
     
     def feedback
@@ -52,6 +60,35 @@ module APNS
     end
     
     protected
+
+    # Connection initialization and notifications sending
+    def self.with_connection
+      attempts = 1
+      
+      begin      
+        # If no @ssl is created or if @ssl is closed we need to start it
+        if @ssl.nil? || @sock.nil? || @ssl.closed? || @sock.closed?
+          @sock, @ssl = self.open_connection
+      end
+    
+      yield
+
+      rescue StandardError, Errno::EPIPE
+        raise unless attempts < @retries
+        @ssl.close
+        @sock.close
+        attempts += 1
+        retry
+      end
+  
+      # Only force close if not persistent
+      unless @persistent
+        @ssl.close
+        @ssl = nil
+        @sock.close
+        @sock = nil
+      end
+    end
 
     def open_connection
       raise "The path to your pem file is not set. (APNS.pem = /path/to/cert.pem)" unless self.pem
@@ -77,7 +114,6 @@ module APNS
       context.key  = OpenSSL::PKey::RSA.new(File.read(self.pem), self.pass)
       
       fhost = self.host.gsub('gateway','feedback')
-      puts fhost
       
       sock         = TCPSocket.new(fhost, 2196)
       ssl          = OpenSSL::SSL::SSLSocket.new(sock, context)
