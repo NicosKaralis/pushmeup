bf4db6297ef5844b2ee93f0c41712be1e336b7dc
Pedro Piñera <pepibumur@gmail.com>
2014-08-07 18:19:50 +0200

    Fixed tests

--- spec/lib/pushmeup_spec.rb
+++ spec/lib/pushmeup_spec.rb
@@ -6,9 +6,6 @@ describe Pushmeup do
   end
 
   describe "APNS" do
-    it "should have a APNS object" do
-      defined?(@APNS).should_not be_false
-    end
     
     it "should not forget the APNS default parameters" do
       puts @APNS
@@ -47,43 +44,36 @@ describe Pushmeup do
     end
   end
   
-  describe "GCM" do
-    it "should have a GCM object" do
-      defined?(GCM).should_not be_false
-    end
-    
-    describe "Notifications" do
-      
+  describe "Notifications" do
+
       before do
         @options = {:data => "dummy data"}
       end
 
       it "should allow only notifications with device_tokens as array" do
         n = GCM::Notification.new("id", @options)
-        n.device_tokens.is_a?(Array).should be_true
+        n.device_tokens.is_a?(Array).should be_truthy
 
         n.device_tokens = ["a" "b", "c"]
-        n.device_tokens.is_a?(Array).should be_true
+        n.device_tokens.is_a?(Array).should be_truthy
 
         n.device_tokens = "a"
-        n.device_tokens.is_a?(Array).should be_true
+        n.device_tokens.is_a?(Array).should be_truthy
       end
 
       it "should allow only notifications with data as hash with :data root" do
-        n = GCM::Notification.new("id", {:data => "data"})
-        
-        n.data.is_a?(Hash).should be_true
+        n = GCM::Notification.new("id", { :data => "data" })
+
+        n.data.is_a?(Hash).should be_truthy
         n.data.should == {:data => "data"}
 
         n.data = {:a => ["a", "b", "c"]}
-        n.data.is_a?(Hash).should be_true
+        n.data.is_a?(Hash).should be_truthy
         n.data.should == {:a => ["a", "b", "c"]}
-        
+
         n.data = {:a => "a"}
-        n.data.is_a?(Hash).should be_true
+        n.data.is_a?(Hash).should be_truthy
         n.data.should == {:a => "a"}
       end
-      
     end
-  end
 end
