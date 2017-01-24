ddff52bcf208b1983a81263787819f4ab18c063d
Marta Noya <marta.noya@est.fib.upc.edu>
2017-01-24 09:55:29 +0100

    Update specs

--- spec/lib/pushmeup_spec.rb
+++ spec/lib/pushmeup_spec.rb
@@ -1,81 +1,70 @@
 require 'spec_helper'
 
 describe Pushmeup do
-  describe "APNS" do
-    it "should have a APNS object" do
-      defined?(APNS).should_not be_false
+  describe 'APNS' do
+    it 'has a APNS object' do
+      expect(defined?(APNS)).to be_truthy
     end
 
-    it "should not forget the APNS default parameters" do
-      APNS.host.should == "gateway.sandbox.push.apple.com"
-      APNS.port.should == 2195
-      APNS.pem.should be_equal(nil)
-      APNS.pass.should be_equal(nil)
+    it 'doesn\'t forget the APNS default parameters' do
+      expect(APNS.host).to eql('gateway.sandbox.push.apple.com')
+      expect(APNS.port).to eql(2195)
+      expect(APNS.pem).to be_nil
+      expect(APNS.pass).to be_nil
     end
 
-    describe "Notifications" do
-
-      describe "#==" do
-
-        it "should properly equate objects without caring about object identity" do
-          a = APNS::Notification.new("123", {:alert => "hi"})
-          b = APNS::Notification.new("123", {:alert => "hi"})
-          a.should eq(b)
+    context 'Notifications' do
+      context '#==' do
+        it 'properly equates objects without caring about object identity' do
+          a = APNS::Notification.new('123', {alert: '123'})
+          b = APNS::Notification.new('123', {alert: '123'})
+          expect(a).to eq(b)
         end
-
       end
-
     end
-
   end
 
-  describe "GCM" do
-    it "should have a GCM object" do
-      defined?(GCM).should_not be_false
+  describe 'GCM' do
+    it 'has a GCM object' do
+      expect(defined?(GCM)).to be_truthy
     end
 
-    describe "Notifications" do
+    context 'Notifications' do
+      let(:options) { {data: 'dummy data'} }
 
-      before do
-        @options = {:data => "dummy data"}
+      it 'allows only notifications with device_tokens as array' do
+        n = GCM::Notification.new('id', options)
+        expect(n.device_tokens).to be_an(Array)
+
+        n.device_tokens = ['a', 'b', 'c']
+        expect(n.device_tokens).to be_an(Array)
+
+        n.device_tokens = 'a'
+        expect(n.device_tokens).to be_an(Array)
       end
 
-      it "should allow only notifications with device_tokens as array" do
-        n = GCM::Notification.new("id", @options)
-        n.device_tokens.is_a?(Array).should be_true
+      it 'allows only notifications with data as hash with :data root' do
+        n = GCM::Notification.new('id', options)
 
-        n.device_tokens = ["a" "b", "c"]
-        n.device_tokens.is_a?(Array).should be_true
+        expect(n.data).to be_a(Hash)
+        expect(n.data).to eql(options)
 
-        n.device_tokens = "a"
-        n.device_tokens.is_a?(Array).should be_true
+        n.data = {a: ['a', 'b', 'c']}
+        expect(n.data).to be_a(Hash)
+        expect(n.data).to eql(a: ['a', 'b', 'c'])
+
+        n.data = {a: "a"}
+        expect(n.data).to be_a(Hash)
+        expect(n.data).to eql(a: 'a')
       end
 
-      it "should allow only notifications with data as hash with :data root" do
-        n = GCM::Notification.new("id", { :data => "data" })
-
-        n.data.is_a?(Hash).should be_true
-        n.data.should == {:data => "data"}
-
-        n.data = {:a => ["a", "b", "c"]}
-        n.data.is_a?(Hash).should be_true
-        n.data.should == {:a => ["a", "b", "c"]}
-
-        n.data = {:a => "a"}
-        n.data.is_a?(Hash).should be_true
-        n.data.should == {:a => "a"}
-      end
-
-      describe "#==" do
-
-        it "should properly equate objects without caring about object identity" do
-          a = GCM::Notification.new("id", { :data => "data" })
-          b = GCM::Notification.new("id", { :data => "data" })
-          a.should eq(b)
+      context '#==' do
+        it 'properly equates objects without caring about object identity' do
+          a = GCM::Notification.new('id', { data: 'data' })
+          b = GCM::Notification.new('id', { data: 'data' })
+          expect(a).to eq(b)
         end
-
       end
-
     end
   end
 end
