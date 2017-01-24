30b101b1f50ca62c10261c84ea40be96377741de
Marta Noya <marta.noya@est.fib.upc.edu>
2017-01-24 09:55:51 +0100

    Add colorful output for specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,1 +1,13 @@
 require 'pushmeup'
+
+RSpec.configure do |config|
+  # Use color in STDOUT
+  config.color = true
+
+  # Use color not only in STDOUT but also in pagers and files
+  config.tty = true
+
+  # Use the specified formatter
+  config.formatter = :documentation # :progress, :html,
+                                    # :json, CustomFormatterClass
+end
