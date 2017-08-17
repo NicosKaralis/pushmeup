40cd9e51e502da11585f7ea02224fcaaa93116bc
Sarah Klinefelter <isuPatches@yahoo.com>
2017-08-17 17:11:51 -0500

    Update GCM again

--- lib/pushmeup/gcm/core.rb
+++ lib/pushmeup/gcm/core.rb
@@ -1,12 +1,9 @@
 require 'httparty'
-# require 'cgi'
 require 'json'
 
 module GCM
   class Application
     include HTTParty
-
-    attr_accessor :host, :format, :key
 
     def initialize(host = 'https://android.googleapis.com/gcm/send', format = :json, key = nil)
       @host = host unless host == nil
@@ -33,19 +30,19 @@ module GCM
 
     def send_notification(device_tokens, data = {}, options = {})
       n = GCM::Notification.new(device_tokens, data, options)
-      self.send_notifications([n])
+      send_notifications([n])
     end
 
     def send_notifications(notifications)
       responses = []
       notifications.each do |n|
-        responses << self.prepare_and_send(n)
+        responses << prepare_and_send(n)
       end
       responses
     end
 
     private
-      def self.prepare_and_send(n)
+      def prepare_and_send(n)
         if n.device_tokens.count < 1 || n.device_tokens.count > 1000
           raise 'Number of device_tokens invalid, keep it betwen 1 and 1000'
         end
@@ -56,16 +53,16 @@ module GCM
           raise %{If your key is a hash of keys you'l need to pass a identifier to the notification!}
         end
 
-        if self.format == :json
-          self.send_push_as_json(n)
-        elsif self.format == :text
-          self.send_push_as_plain_text(n)
+        if @format == :json
+          send_push_as_json(n)
+        elsif @format == :text
+          send_push_as_plain_text(n)
         else
           raise 'Invalid format'
         end
       end
 
-      def self.send_push_as_json(n)
+      def send_push_as_json(n)
         headers = {
           'Authorization' => "key=#{ self.key(n.identity) }",
           'Content-Type' => 'application/json',
@@ -77,26 +74,26 @@ module GCM
           :time_to_live => n.time_to_live,
           :delay_while_idle => n.delay_while_idle
         }
-        return self.send_to_server(headers, body.to_json)
+        send_to_server(headers, body.to_json)
       end
 
-      def self.send_push_as_plain_text(n)
+      def send_push_as_plain_text(n)
         raise 'Still has to be done: http://developer.android.com/guide/google/gcm/gcm.html'
         headers = {
           # TODO: Aceitar key ser um hash
-          'Authorization' => "key=#{ self.key(n.identity) }",
+          'Authorization' => "key=#{ key(n.identity) }",
           'Content-Type' => 'application/x-www-form-urlencoded;charset=UTF-8',
         }
-        return self.send_to_server(headers, body)
+        send_to_server(headers, body)
       end
 
-      def self.send_to_server(headers, body)
-        params = {:headers => headers, :body => body}
-        response = self.post(self.host, params)
-        return build_response(response)
+      def send_to_server(headers, body)
+        params = { headers: headers, body: body}
+        response = post(@host, params)
+        build_response(response)
       end
 
-      def self.build_response(response)
+      def build_response(response)
         case response.code
           when 200
             {:response =>  'success', :body => JSON.parse(response.body), :headers => response.headers, :status_code => response.code}
@@ -108,6 +105,8 @@ module GCM
             {:response => 'There was an internal error in the GCM server while trying to process the request.', :status_code => response.code}
           when 503
             {:response => 'Server is temporarily unavailable.', :status_code => response.code}
+          else
+            # Do nothing
         end
       end
   end
